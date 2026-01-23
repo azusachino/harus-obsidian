eb5b5c526b7df576b276b4d986197cd2eb5fbcc3
haru <azusachino@proton.me>
2026-01-23 10:48:54 +0900

    feat: enable spa to test performance

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "HarusObsidian",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
